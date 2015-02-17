f375591d751f86d44908b823b117ed2616695584
TivsLThree <randihuchingson@Randis-MacBook-Pro.local>
2015-02-17 16:06:52 -0500

    Added a pause button for BuildSeason and did stuff
    http://buttonoptimizer.com/#

--- FRC SIM/src/frc/sim/BuildSeasonState.java
+++ FRC SIM/src/frc/sim/BuildSeasonState.java
@@ -10,45 +10,51 @@ import java.awt.event.MouseEvent;
 import javax.swing.Timer;
 
 public class BuildSeasonState extends State {
-    
+
     public Day[] days = new Day[]{Day.SATURDAY, Day.SUNDAY, Day.MONDAY, Day.TUESDAY, Day.WEDNESDAY, Day.THURSDAY, Day.FRIDAY};
     public Day day = Day.SATURDAY;
     public int week = 1;
     public Person selected = null;
     public Timer timer;
-    
+    public double code = 0;
+    public boolean pause = true;
+
     public BuildSeasonState() {
         int seperation = 130;
         int start = 10;
         timer = new Timer(2000, (ActionEvent e) -> {
-            int index = 0;
-            for (Day d : days) {
-                if (day == d) {
-                    break;
+            if (!pause) {
+                int index = 0;
+                for (Day d : days) {
+                    if (day == d) {
+                        break;
+                    }
+                    index++;
                 }
-                index++;
-            }
-            if ((index + 1) < days.length) {
-                day = days[index + 1];
-            } else {
-                week++;
-                if (week != 6) {
-                    day = Day.SATURDAY;
+                if ((index + 1) < days.length) {
+                    daily();
+                    day = days[index + 1];
                 } else {
-                    //TODO go to CompetitionState
+                    week++;
+                    if (week != 6) {
+                        day = Day.SATURDAY;
+                    } else {
+                        //TODO go to CompetitionState
+                    }
                 }
-            }           
+            }
         });
+
         timer.start();
-        
-        entities.add(new Button(start, 50, "buttons/programming.png") {
+
+        entities.add(new Button(start, 50, "buttons/button-3.png") {
             @Override
             public void clicked(MouseEvent m) {
                 if (selected != null && selected.role != Type.MENTOR) {
                     selected.role = Type.PROGRAMMING;
                 }
             }
-            
+
             @Override
             public void tick() {
                 super.tick();
@@ -66,7 +72,7 @@ public class BuildSeasonState extends State {
                     selected.role = Type.MECHANICAL;
                 }
             }
-            
+
             @Override
             public void tick() {
                 super.tick();
@@ -75,7 +81,7 @@ public class BuildSeasonState extends State {
                 } else {
                     visible = true;
                 }
-            }        
+            }
         });
         entities.add(new Button(start + (seperation * 2), 50, "buttons/electrical.png") {
             @Override
@@ -84,7 +90,7 @@ public class BuildSeasonState extends State {
                     selected.role = Type.ELECTRICAL;
                 }
             }
-            
+
             @Override
             public void tick() {
                 super.tick();
@@ -102,7 +108,7 @@ public class BuildSeasonState extends State {
                     selected.role = Type.PR;
                 }
             }
-            
+
             @Override
             public void tick() {
                 super.tick();
@@ -113,20 +119,41 @@ public class BuildSeasonState extends State {
                 }
             }
         });
+        entities.add(new Button(600, 50, "buttons/unpause.png") {
+
+            @Override
+            public void clicked(MouseEvent m) {
+                pause = !pause;
+                
+            }
+
+            @Override
+            public void tick() {
+                super.tick();
+                visible = true;
+
+            }
+        });
     }
-    
+
     @Override
     public void draw(Graphics2D g) {
         super.draw(g);
         g.drawString(day.toString() + ", Week " + week, 10, 20);
         for (Person p : Main.team.members) {
+
             p.draw(g);
             p.tick();
         }
+
         if (selected != null) {
             Rectangle r = selected.box();
             Color oldColor = g.getColor();
+            g.drawString(selected.preferredRole.toString(), selected.x, selected.y);
+            g.drawString(selected.role.toString(), selected.x, selected.y + 32);
+            g.drawString(String.valueOf(selected.skill), selected.x - 32, selected.y + 16);
             g.setColor(Color.GREEN);
+
             g.drawRect(r.x, r.y, r.width, r.height);
             g.setColor(oldColor);
         }
@@ -147,6 +174,7 @@ public class BuildSeasonState extends State {
     }
 
     public enum Day {
+
         SUNDAY,
         MONDAY,
         TUESDAY,
@@ -154,5 +182,18 @@ public class BuildSeasonState extends State {
         THURSDAY,
         FRIDAY,
         SATURDAY;
+    }
+
+    public void daily() {
+        doCode();
+        System.out.println(code);
+    }
+
+    public void doCode() {
+        for (Person p : Main.team.members) {
+            if (p.role == Type.PROGRAMMING) {
+                code += (p.preferredRole != Type.PROGRAMMING && p.preferredRole != Type.NONE) ? (1 * p.skill * 0.9) : (1 * p.skill);
+            }
+        }
     }
 }
